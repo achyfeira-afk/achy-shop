f2a57d010972e60c8af2922d2eb705c13ea9e1a5
achyfeira-afk <achyfeira@gmail.com>
2026-04-15 00:21:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
-
 const productList = document.getElementById("productList");
 const searchInput = document.getElementById("searchInput");
 
-const path = window.location.pathname.toLowerCase();
+const page = window.location.href;
 
-let category = "abaya";
+let category = "";
 
-if (path.includes("fashion")) {
+if (page.includes("index.html") || page.endsWith("/") || page.includes("abaya")) {
+  category = "abaya";
+}
+else if (page.includes("fashion")) {
   category = "fashion";
 }
-else if (path.includes("jaket")) {
+else if (page.includes("jaket")) {
   category = "jaket";
 }
 
-function render(keyword = "") {
+const filteredProducts = products.filter(p => p.category === category);
 
-  const filtered = products.filter(p => {
-    const matchCategory = p.category === category;
-    const matchSearch = p.name.toLowerCase().includes(keyword.toLowerCase());
-    return matchCategory && matchSearch;
-  });
-
-  productList.innerHTML = filtered.map(p => `
+function render(list) {
+  productList.innerHTML = list.map(p => `
     <div class="card">
-      <img src="${p.image}" alt="${p.name}">
-      <div class="card-content">
-        <h3>${p.name}</h3>
-        <p>${p.price}</p>
-        <a class="btn-shopee" href="${p.link}" target="_blank">Beli Sekarang</a>
-      </div>
+      <img src="${p.image}">
+      <h3>${p.name}</h3>
+      <p>${p.price}</p>
+      <a class="btn-shopee" href="${p.link}" target="_blank">Beli Sekarang</a>
     </div>
   `).join("");
 }
 
-render();
+render(filteredProducts);
 
 if (searchInput) {
-  searchInput.addEventListener("input", (e) => {
-    render(e.target.value);
+  searchInput.addEventListener("input", function () {
+    const keyword = this.value.toLowerCase();
+
+    const filtered = filteredProducts.filter(p =>
+      p.name.toLowerCase().includes(keyword)
+    );
+
+    render(filtered);
   });
 }
